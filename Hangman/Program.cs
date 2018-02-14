09628beed081c1de89cd63b7043350bf2fe532ce
Kino Kashi <kinodekirakashi@live.com>
2018-02-14 13:44:50 -0600

    add playability

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -11,9 +11,55 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
+            Start:
+
             //Variables
             Answers a = new Answers();
             string answer = a.Answer;
+            string guess;
+            string[] correctGuesses = new string[answer.Length];
+            int chances = 6;
+
+            //Introduction
+            Console.WriteLine("Welcome to Hangman!");
+            Console.WriteLine();
+            Console.WriteLine("The word you are guessing has " + answer.Length + " letters!");
+
+
+            Guess:
+            Console.WriteLine();
+            Console.WriteLine("Guess a letter!");
+ 
+            //Variables based on guess
+            guess = Console.ReadLine();
+            int letterIndex = answer.IndexOf(guess);
+
+            //Checks to see if guess is part of the answer, otherwise takes a life
+            if (answer.Contains(guess))
+            {
+                correctGuesses[letterIndex] = guess;
+                Console.WriteLine("That is letter number " + (letterIndex + 1) + "!");
+                Console.WriteLine("So far you have: " + "[{0}]", string.Join(", ", correctGuesses));
+                Console.WriteLine("And " + chances + " more chances!");
+                Console.WriteLine();
+                goto Guess;
+            }
+            else {
+                chances--;
+                Console.WriteLine();
+                Console.WriteLine("Sorry that is incorrect! You have " + chances + " chances left");
+
+                //Ends game when lives reach 0
+                if (chances == 0) {
+                    Console.WriteLine("You have run out of guesses! The word was '" + answer + "'");
+                    Console.WriteLine("Press any key to play again");
+                    Console.ReadKey();
+                    goto Start;
+                }
+                else {
+                    goto Guess;
+                }
+            }
         }
     }
 }
